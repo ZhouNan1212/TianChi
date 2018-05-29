d59f448eb568c34a2ef94bfda6a12d35b4ec7a9d
周楠 <906037502@qq.com>
2018-05-29 19:33:42 +0800

    创建TianChi代码库，以及加入读取HTML文件的代码

---
+++ InformationExtraction.py
@@ -0,0 +1,10 @@
+
+
+def get_text_from_html(html):
+    print('ads')
+
+
+if __name__ == "__main__":
+    with open(r"Data\information_ectraction\增减持\html\6927.html", "r", encoding='utf-8') as f:
+        text = f.read()
+        print(text)
